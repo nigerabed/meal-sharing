9e4022fb9b4e5e3c5ee7d3369bda526e8745d520
nigerabed <nigerabed@gmail.com>
2025-07-04 00:15:34 +0200

    added logic of displaying registration form

--- app-next/app/page.js
+++ app-next/app/page.js
@@ -1,14 +1,12 @@
 "use client";
 import styles from "./page.module.css"
-
-// import HomePage from "@/components/HomePage/HomePage";
 import MealList from "../components/MealsList/MealList";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      {/* <HomePage /> */}
+   
       <h1 className={styles.heading}>Welcome to MealTime!</h1>
       <p className={styles.text}>Discover delicious meals from around the world</p>
       <div className={styles.meals}>
